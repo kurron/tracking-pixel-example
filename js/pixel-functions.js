6a12a0f438a86917e01cfcffe1fb854040ef7c2d
Ron Kurr <kurr@kurron.org>
2018-04-12 14:05:57 -0400

    Added support for calling the tracking pixel via POST.

--- js/pixel-functions.js
+++ js/pixel-functions.js
@@ -1,10 +1,23 @@
-function adpTrack( json ) {
-    console.debug( "track called" );
+function adpTrackGet( json ) {
+    console.debug( "adpTrackGet called" );
     const data = toQueryString( json );
     const img = new Image();
     img.src = encodeURI( 'https://nkkmfsfq10.execute-api.us-west-1.amazonaws.com/adpevents/?' + data );
     img.style.display = 'none';
     document.body.appendChild( img );
+}
+
+function adpTrackPost( json ) {
+    console.debug( "adpTrackPost called" );
+    var xhttp = new XMLHttpRequest();
+    xhttp.open( "POST", "https://nkkmfsfq10.execute-api.us-west-1.amazonaws.com/adpevents/", true );
+    //xhttp.open( "POST", "http://httpbin.org/post/", true );
+    xhttp.setRequestHeader( "Content-Type", "application/json" );
+    xhttp.setRequestHeader( "Accept", "application/json" );
+    let data = JSON.stringify( json );
+    console.debug( "data = " + data );
+    xhttp.send( data );
+    console.debug( "Response = " + xhttp.responseText );
 }
 
 function toQueryString( json ) {
